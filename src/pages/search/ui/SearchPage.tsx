021aeccf7bb6969d9ccb96ba488fba3f26b0348c
Alena Alioshyna <elenakovalski1995@yandex.ru>
2026-01-31 13:28:26 +0300

    add LinearProgress + Skeleton from "react-loading-skeleton"

--- src/pages/search/ui/SearchPage.tsx
+++ src/pages/search/ui/SearchPage.tsx
@@ -1,9 +1,7 @@
 import { useSearchParams } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-
 import styles from "./SearchPage.module.css";
-
 import { SearchForm } from "@/features/search-movie/ui/SearchForm";
 import { MovieCard } from "@/entities/movie/ui/MovieCard/MovieCard";
 import { useGetSearchMoviesInfiniteQuery } from "@/entities/movie/api/tmdbMovieApi";
